591eed39a0fb1b0dd989bcced3b20042e35dd0fd
congenial-course-2234 <jakelozano94@gmail.com>
2020-05-13 20:25:11 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,12 +1,9 @@
 # don't forget to add: require 'pry'
 require 'pry'
 
-
 def snake_it_up(string)
-  count = 0
-  if string[0] == "s" && count < 10
-    "s" + string
-    count += 1
+  if string[0] == "s"
+   10 * "s" + string
   else
     string
   end
